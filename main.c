38ee0866f85c0d3f7ff289d22a6dd71af049189c
sebub <105357150+sebub@users.noreply.github.com>
2022-11-17 21:23:46 +0100

    hj

--- main.c
+++ main.c
@@ -16,10 +16,7 @@ int main() {
         printf("%d \t%d \t%d \t%d\n", co2, cost, time, remainder);
 
         scanf("%s \t%d",input, &value);
-
-
         convert_to_lowercase(input);
-
         if(strcmp(input,"env")==0)
             co2 +=value;
         else if(strcmp(input,"cost")==0)
@@ -41,6 +38,5 @@ int main() {
 void convert_to_lowercase(char *str){//runs over every letter in the string and converts them to lowercase
     for (int i = 0; i < strlen(str); ++i) {
         str[i]= tolower(str[i]);
-
     }
 }
