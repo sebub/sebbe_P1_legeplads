dd53301998a133f9cd571c7a2a7bb956e9b8bb5c
sebub <105357150+sebub@users.noreply.github.com>
2022-11-21 16:39:32 +0100

    df

--- main.c
+++ main.c
@@ -4,40 +4,19 @@
 
 int find_number_of_cities();
 //void create_city_name_array(char city_name_array[20][20]);
+char** create_city_name_array(int number_of_cities);
 
 int main() {
 
 
     int nr = find_number_of_cities();
+    char** cityname = create_city_name_array(nr);
+    int** city_distance_array =
 
-
-    char city_name_array[nr][20];
-
-    FILE *city_data = fopen("cities.txt","r");
-    if(city_data == NULL){
-        printf("file not found");
-    }
-
-    char temp1[30];
-    char prev_city[30];
-    int index = 0;
-
-    while(fscanf(city_data,"%s %*s %*d",temp1)==1){
-
-        if(strcmp(temp1,prev_city) != 0) {
-            strcpy(city_name_array[index],temp1);
-            index++;
-        }
-        strcpy(prev_city,temp1);
-    }
-
-    fclose(city_data);
-
-    printf("%d\n",nr);
 
     for (int i = 0; i < nr; ++i) {
 
-        printf("%s\n",city_name_array[i]);
+        printf("%s\n",cityname[i]);
     }
 
 
@@ -64,27 +43,32 @@ int find_number_of_cities(){
     return number_of_cities;
 }
 
-/*void create_city_name_array(char city_name_array[20][20]){
+char** create_city_name_array(int number_of_cities){
     FILE *city_data = fopen("cities.txt","r");
     if(city_data == NULL){
         printf("file not found");
     }
-
-    char temp1[30];
-    char prev_city[30];
+    char **citynames;
+    citynames = malloc(number_of_cities * sizeof(char*));
+    char temp1[20];
+    char prev_city[20];
     int index = 0;
 
+
     while(fscanf(city_data,"%s %*s %*d",temp1)==1){
-
-
         if(strcmp(temp1,prev_city) != 0) {
-            printf("\n\tNumber: %d City: %s\n", index,temp1);
-            strcpy(city_name_array[index],temp1);
+            citynames[index] = malloc((strlen(temp1)*sizeof(char)));
+            strcpy(citynames[index],temp1);
             index++;
+
         }
         strcpy(prev_city,temp1);
     }
 
     fclose(city_data);
+    return citynames;
+}
 
-}*/
+int** create_djikstra_array(int number_of_cities){
+    
+}
